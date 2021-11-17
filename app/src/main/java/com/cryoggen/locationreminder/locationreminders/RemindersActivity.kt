0d04b3c48dee6e97069490c5322bb0b52ce13015
Sergey-Svetlov <cryoggen@gmail.com>
2021-11-17 21:25:54 +0300

    Create RemindersActivity

---
+++ app/src/main/java/com/cryoggen/locationreminder/locationreminders/RemindersActivity.kt
@@ -0,0 +1,12 @@
+package com.cryoggen.locationreminder.locationreminders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cryoggen.locationreminder.R
+
+class RemindersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminders)
+    }
+}
