6084460af9b4f1f6544ed5bcecc5c3bca8874e8f
Sergey-Svetlov <cryoggen@gmail.com>
2021-11-19 21:07:02 +0300

    Create FragmentReminders

---
+++ app/src/main/java/com/cryoggen/locationreminder/locationreminders/FragmentRemindersViewModel.kt
@@ -0,0 +1,7 @@
+package com.cryoggen.locationreminder.locationreminders
+
+import androidx.lifecycle.ViewModel
+
+class FragmentRemindersViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
