4b2540b47a27a711ce7300319d19fe811b10c13f
Sergey-Svetlov <cryoggen@gmail.com>
2021-11-20 23:35:16 +0300

    feat: Create class RemindersLocalDataSource

---
+++ app/src/main/java/com/cryoggen/locationreminder/data/local/RemindersDataSource.kt
@@ -0,0 +1,39 @@
+package com.cryoggen.locationreminder.data.local
+
+import androidx.lifecycle.LiveData
+import com.cryoggen.locationreminder.data.Reminder
+
+/**
+ * Main entry point for accessing Reminders data.
+ */
+interface RemindersDataSource {
+
+
+    fun observeReminders(): LiveData<Result<List<Reminder>>>
+
+    suspend fun getReminders(): Result<List<Reminder>>
+
+    suspend fun refreshReminders()
+
+    fun observeReminder(ReminderId: String): LiveData<Result<Reminder>>
+
+    suspend fun getReminder(ReminderId: String): Result<Reminder>
+
+    suspend fun refreshReminder(ReminderId: String)
+
+    suspend fun saveReminder(Reminder: Reminder)
+
+    suspend fun completeReminder(Reminder: Reminder)
+
+    suspend fun completeReminder(ReminderId: String)
+
+    suspend fun activateReminder(Reminder: Reminder)
+
+    suspend fun activateReminder(ReminderId: String)
+
+    suspend fun clearCompletedReminders()
+
+    suspend fun deleteAllReminders()
+
+    suspend fun deleteReminder(ReminderId: String)
+}
