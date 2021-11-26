89d20a93c8dc949a2eb9afb233d3b390e305edac
Sergey-Svetlov <cryoggen@gmail.com>
2021-11-26 22:52:49 +0300

    Update MainActivity.kt

--- app/src/main/java/com/cryoggen/locationreminder/MainActivity.kt
+++ app/src/main/java/com/cryoggen/locationreminder/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cryoggen.locationreminder
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,3 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+const val ADD_EDIT_RESULT_OK = Activity.RESULT_FIRST_USER + 1
+const val DELETE_RESULT_OK = Activity.RESULT_FIRST_USER + 2
+const val EDIT_RESULT_OK = Activity.RESULT_FIRST_USER + 3
