99b18b1513973705b064cd5b1c11ab9e30ec22a4
Sergey-Svetlov <cryoggen@gmail.com>
2021-11-28 18:31:10 +0300

    feat: Create StatisticsFragment.kt

---
+++ app/src/main/java/com/cryoggen/locationreminder/statistics/StatisticsFragment.kt
@@ -0,0 +1,41 @@
+package com.cryoggen.locationreminder.statistics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.cryoggen.locationreminder.R
+import com.cryoggen.locationreminder.databinding.StatisticsFragBinding
+import com.cryoggen.locationreminder.reminders.util.setupRefreshLayout
+import com.example.android.architecture.blueprints.todoapp.statistics.StatisticsViewModel
+
+/**
+ * Main UI for the statistics screen.
+ */
+class StatisticsFragment : Fragment() {
+
+    private lateinit var viewDataBinding: StatisticsFragBinding
+
+    private val viewModel by viewModels<StatisticsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = DataBindingUtil.inflate(
+            inflater, R.layout.statistics_frag, container,
+            false
+        )
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewDataBinding.viewmodel = viewModel
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+    }
+}
