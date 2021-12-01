1eae34df826f5cc82f2af275090584b01f4c96aa
Sergey-Svetlov <cryoggen@gmail.com>
2021-12-01 22:56:29 +0300

    fix: A bug fix

--- app/src/main/java/com/cryoggen/locationreminder/locationreminders/FragmentRemindersViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.cryoggen.locationreminder.locationreminders
-
-import androidx.lifecycle.ViewModel
-
-class FragmentRemindersViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
