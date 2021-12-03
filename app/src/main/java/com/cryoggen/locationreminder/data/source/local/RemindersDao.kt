a1c88591b7197fe49fb701d7850dc510fb90e3e8
Sergey-Svetlov <cryoggen@gmail.com>
2021-12-03 21:19:42 +0300

    Update RemindersDao.kt

--- app/src/main/java/com/cryoggen/locationreminder/data/source/local/RemindersDao.kt
+++ app/src/main/java/com/cryoggen/locationreminder/data/source/local/RemindersDao.kt
@@ -1,4 +1,3 @@
-
 package com.cryoggen.locationreminder.data.source.local
 
 import androidx.lifecycle.LiveData
@@ -29,8 +28,8 @@ interface RemindersDao {
      * @param ReminderId the Reminder id.
      * @return the Reminder with ReminderId.
      */
-    @Query("SELECT * FROM Reminders WHERE entryid = :ReminderId")
-    fun observeReminderById(ReminderId: String): LiveData<Reminder>
+    @Query("SELECT * FROM Reminders WHERE entryid = :reminderId")
+     fun observeReminderById(reminderId: String): LiveData<Reminder>
 
     /**
      * Select all Reminders from the Reminders table.
@@ -38,7 +37,7 @@ interface RemindersDao {
      * @return all Reminders.
      */
     @Query("SELECT * FROM Reminders")
-    suspend fun getReminders(): List<Reminder>
+     fun getReminders(): List<Reminder>
 
     /**
      * Select a Reminder by id.
@@ -46,8 +45,8 @@ interface RemindersDao {
      * @param ReminderId the Reminder id.
      * @return the Reminder with ReminderId.
      */
-    @Query("SELECT * FROM Reminders WHERE entryid = :ReminderId")
-    suspend fun getReminderById(ReminderId: String): Reminder?
+    @Query("SELECT * FROM Reminders WHERE entryid = :reminderId")
+    fun getReminderById(reminderId: String): Reminder?
 
     /**
      * Insert a Reminder in the database. If the Reminder already exists, replace it.
@@ -55,7 +54,7 @@ interface RemindersDao {
      * @param Reminder the Reminder to be inserted.
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertReminder(Reminder: Reminder)
+    suspend fun insertReminder(reminder: Reminder)
 
     /**
      * Update a Reminder.
@@ -64,7 +63,7 @@ interface RemindersDao {
      * @return the number of Reminders updated. This should always be 1.
      */
     @Update
-    suspend fun updateReminder(Reminder: Reminder): Int
+    fun updateReminder(reminder: Reminder): Int
 
     /**
      * Update the complete status of a Reminder
@@ -72,22 +71,22 @@ interface RemindersDao {
      * @param ReminderId    id of the Reminder
      * @param completed status to be updated
      */
-    @Query("UPDATE Reminders SET completed = :completed WHERE entryid = :ReminderId")
-    suspend fun updateCompleted(ReminderId: String, completed: Boolean)
+    @Query("UPDATE Reminders SET completed = :completed WHERE entryid = :reminderId")
+     fun updateCompleted(reminderId: String, completed: Boolean)
 
     /**
      * Delete a Reminder by id.
      *
      * @return the number of Reminders deleted. This should always be 1.
      */
-    @Query("DELETE FROM Reminders WHERE entryid = :ReminderId")
-    suspend fun deleteReminderById(ReminderId: String): Int
+    @Query("DELETE FROM Reminders WHERE entryid = :reminderId")
+     fun deleteReminderById(reminderId: String): Int
 
     /**
      * Delete all Reminders.
      */
-    @Query("DELETE FROM Reminders")
-    suspend fun deleteReminders()
+    @Query("DELETE FROM reminders")
+    fun deleteReminders()
 
     /**
      * Delete all completed Reminders from the table.
@@ -95,5 +94,5 @@ interface RemindersDao {
      * @return the number of Reminders deleted.
      */
     @Query("DELETE FROM Reminders WHERE completed = 1")
-    suspend fun deleteCompletedReminders(): Int
+   fun deleteCompletedReminders(): Int
 }
