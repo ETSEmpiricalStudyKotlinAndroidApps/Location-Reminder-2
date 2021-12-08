c3d704e77dc23b4dc77f6f4335441d7a58b4dbcd
Sergey-Svetlov <cryoggen@gmail.com>
2021-12-08 21:40:31 +0300

    Update Reminder.kt

--- app/src/main/java/com/cryoggen/locationreminder/data/Reminder.kt
+++ app/src/main/java/com/cryoggen/locationreminder/data/Reminder.kt
@@ -10,6 +10,7 @@ import java.util.UUID
 data class Reminder @JvmOverloads constructor(
     @ColumnInfo(name = "title") var title: String = "",
     @ColumnInfo(name = "description") var description: String = "",
+    @ColumnInfo(name = "useruid") var userUID: String = "",
     @ColumnInfo(name = "completed") var isCompleted: Boolean = false,
     @PrimaryKey @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString()
 ) {
