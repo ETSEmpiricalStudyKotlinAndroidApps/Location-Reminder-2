1f8db0946109aebf8736bdffd556245789370ab5
Sergey-Svetlov <cryoggen@gmail.com>
2021-12-10 16:40:11 +0300

    Create ExitFragment.kt

---
+++ app/src/main/java/com/cryoggen/locationreminder/exit/ExitFragment.kt
@@ -0,0 +1,27 @@
+package com.cryoggen.locationreminder.exit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.cryoggen.locationreminder.R
+import com.cryoggen.locationreminder.reminders.RemindersFragmentDirections
+import com.firebase.ui.auth.AuthUI
+
+class ExitFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        AuthUI.getInstance().signOut(requireContext())
+        val action = ExitFragmentDirections
+            .actionExitFragmentToLoginFragment()
+        findNavController().navigate(action)
+        return inflater.inflate(R.layout.fragment_exit, container, false)
+    }
+
+
+}
