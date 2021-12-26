19bbdd34406217aec05d918e916f675fbb78361d
Sergey-Svetlov <cryoggen@gmail.com>
2021-12-26 22:09:42 +0300

    Update MainActivity.kt

--- app/src/main/java/com/cryoggen/locationreminder/MainActivity.kt
+++ app/src/main/java/com/cryoggen/locationreminder/MainActivity.kt
@@ -3,8 +3,11 @@ package com.cryoggen.locationreminder
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -15,7 +18,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.cryoggen.locationreminder.map.ConstantsPermissions
+import com.cryoggen.locationreminder.map.checkDeviceLocationSettingsAndStartGeofence
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         lateinit var activity: AppCompatActivity
     }
 
-    private val REQUEST_LOCATION_PERMISSION = 1
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activity = this
@@ -61,9 +67,43 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        val fragment = navHostFragment?.childFragmentManager?.fragments?.get(0)!!
-        fragment.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ConstantsPermissions.REQUEST_TURN_DEVICE_LOCATION_ON) {
+            checkDeviceLocationSettingsAndStartGeofence(false)
+            return
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d(ConstantsPermissions.TAG, "onRequestPermissionResult")
+
+        if (
+            grantResults.isEmpty() ||
+            grantResults[ConstantsPermissions.LOCATION_PERMISSION_INDEX] == PackageManager.PERMISSION_DENIED ||
+            (requestCode == ConstantsPermissions.REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE &&
+                    grantResults[ConstantsPermissions.BACKGROUND_LOCATION_PERMISSION_INDEX] ==
+                    PackageManager.PERMISSION_DENIED)
+        ) {
+
+            Snackbar.make(
+                findViewById(R.id.drawer_layout),
+                R.string.permission_denied_explanation,
+                Snackbar.LENGTH_INDEFINITE
+            )
+                .setAction(R.string.settings) {
+                    startActivity(Intent().apply {
+                        action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    })
+                }.show()
+        } else {
+            checkDeviceLocationSettingsAndStartGeofence()
+        }
     }
 
 }
