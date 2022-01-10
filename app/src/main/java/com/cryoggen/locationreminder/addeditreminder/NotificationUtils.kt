bcb449645d1c71138f613844f547a697dc2fdcfd
Sergey-Svetlov <cryoggen@gmail.com>
2022-01-10 23:21:50 +0300

    Update NotificationUtils.kt

--- app/src/main/java/com/cryoggen/locationreminder/addeditreminder/NotificationUtils.kt
+++ app/src/main/java/com/cryoggen/locationreminder/addeditreminder/NotificationUtils.kt
@@ -1,5 +1,6 @@
 package com.cryoggen.locationreminder.addeditreminder
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -44,22 +45,24 @@ fun createChannel(context: Context) {
  * entered notification.  It sends a custom notification based on the name string associated
  * with the LANDMARK_DATA from GeofencingConstatns in the GeofenceUtils file.
  */
-fun NotificationManager.sendGeofenceEnteredNotification(context: Context, foundIndex: String) {
+fun NotificationManager.sendGeofenceEnteredNotification(context: Context, GeofenceId: String) {
     val contentIntent = Intent(context, MainActivity::class.java)
-    contentIntent.putExtra(GeofencingConstants.EXTRA_GEOFENCE_INDEX, foundIndex)
+    contentIntent.putExtra(GeofencingConstants.EXTRA_GEOFENCE_INDEX, GeofenceId)
     val contentPendingIntent = PendingIntent.getActivity(
         context,
         NOTIFICATION_ID,
         contentIntent,
         PendingIntent.FLAG_UPDATE_CURRENT
     )
-    val mapImage = BitmapFactory.decodeResource(
-        context.resources,
-        R.drawable.map_small
+    val closeNotificationIntent = Intent(context, GeofenceBroadcastReceiver::class.java)
+    closeNotificationIntent.action = GeofencingConstants.ACTION_CLOSE_NOTIFICATION
+    val closePendingIntent = PendingIntent.getBroadcast(
+        context,
+        111,
+        closeNotificationIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
     )
-    val bigPicStyle = NotificationCompat.BigPictureStyle()
-        .bigPicture(mapImage)
-        .bigLargeIcon(null)
+
 
     // We use the name resource ID from the LANDMARK_DATA along with content_text to create
     // a custom message when a Geofence triggers.
@@ -67,20 +70,28 @@ fun NotificationManager.sendGeofenceEnteredNotification(context: Context, foundI
         .setContentTitle(context.getString(R.string.app_name))
         .setContentText(
             context.getString(
-                R.string.content_text,
-                "test"
+                R.string.content_text
             )
         )
         .setPriority(NotificationCompat.PRIORITY_HIGH)
         .setContentIntent(contentPendingIntent)
-        .setSmallIcon(R.drawable.map_small)
-        .setStyle(bigPicStyle)
-        .setLargeIcon(mapImage)
-
-    notify(NOTIFICATION_ID, builder.build())
+        .addAction(
+            R.drawable.ic_notification_open_button_foreground,
+            context.resources.getString(R.string.open), contentPendingIntent
+        )
+        .addAction(
+            R.drawable.ic_notification_close_button_foreground,
+            context.resources.getString(R.string.close), closePendingIntent
+        )
+        .setSmallIcon(R.drawable.ic_notification)
+        .setAutoCancel(true)
+        .setColor(context.resources.getColor(R.color.secondaryDarkColor))
+    val tst = builder.build()
+//    tst.flags = Notification.FLAG_NO_CLEAR or Notification.FLAG_ONGOING_EVENT
+    notify(NOTIFICATION_ID, tst)
 }
 
-private const val NOTIFICATION_ID = 33
+const val NOTIFICATION_ID = 33
 private const val CHANNEL_ID = "GeofenceChannel"
 
 
