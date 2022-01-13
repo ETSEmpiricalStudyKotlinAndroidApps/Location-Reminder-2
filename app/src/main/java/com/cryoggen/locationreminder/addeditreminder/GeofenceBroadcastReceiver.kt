f7ec83fcc43ce094668ecb760f7436d965e41e8c
Sergey-Svetlov <cryoggen@gmail.com>
2022-01-13 22:03:24 +0300

    Update GeofenceBroadcastReceiver.kt

--- app/src/main/java/com/cryoggen/locationreminder/addeditreminder/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/cryoggen/locationreminder/addeditreminder/GeofenceBroadcastReceiver.kt
@@ -1,26 +1,17 @@
 package com.cryoggen.locationreminder.addeditreminder
 
-import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
 import android.util.Log
 import androidx.core.content.ContextCompat
 import com.cryoggen.locationreminder.R
 import com.cryoggen.locationreminder.addeditreminder.GeofencingConstants.ACTION_CLOSE_NOTIFICATION
 import com.cryoggen.locationreminder.addeditreminder.GeofencingConstants.ACTION_GEOFENCE_EVENT
+import com.cryoggen.locationreminder.sound.Sound
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
-import android.media.MediaPlayer
-import android.os.Build
-import android.os.VibrationEffect
-import android.os.Vibrator
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.getSystemService
-import com.cryoggen.locationreminder.main.MainActivity
-import java.lang.Exception
 
 
 /*
@@ -32,22 +23,24 @@ import java.lang.Exception
  * message associated with each Geofence.
  */
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
-    //    private var mediaPlayer = MediaPlayer.create(MainActivity.activity, R.raw.rington)
-    private var mediaPlayer: MediaPlayer? = null
-    private val vibrator = MainActivity.activity.getSystemService<Vibrator>()
 
     override fun onReceive(context: Context, intent: Intent) {
+
+        val intentService = Intent(context, Sound::class.java)
+
         if (intent.action == ACTION_CLOSE_NOTIFICATION) {
+            //stop work service for sound
+            context.stopService(intentService)
+
             val manager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.cancel(NOTIFICATION_ID)
-            releaseMediaPlayer()
-            stopVibratePhone()
+
         }
+
         if (intent.action == ACTION_GEOFENCE_EVENT) {
-            releaseMediaPlayer()
-//            startMediaPlayer()
-            vibratePhone()
+
+
             val geofencingEvent = GeofencingEvent.fromIntent(intent)
 
             if (geofencingEvent.hasError()) {
@@ -59,6 +52,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
             if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
                 Log.v(TAG, context.getString(R.string.geofence_entered))
 
+
                 val GeofenceId = when {
                     geofencingEvent.triggeringGeofences.isNotEmpty() ->
                         geofencingEvent.triggeringGeofences[0].requestId
@@ -67,75 +61,23 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
                         return
                     }
                 }
-                // Check geofence against the constants listed in GeofenceUtil.kt to see if the
-                // user has entered any of the locations we track for geofences.
-//                val foundIndex = GeofencingConstants.LANDMARK_DATA.indexOfFirst {
-//                    it.id == fenceId
-//                }
-//
-//                // Unknown Geofences aren't helpful to us
-//                if ( -1 == foundIndex ) {
-//                    Log.e(TAG, "Unknown Geofence: Abort Mission")
-//                    return
-//                }
 
                 val notificationManager = ContextCompat.getSystemService(
                     context,
                     NotificationManager::class.java
                 ) as NotificationManager
-
+                notificationManager.cancelNotifications()
                 notificationManager.sendGeofenceEnteredNotification(
                     context, GeofenceId
                 )
+                //start work service for sound
+                context.startService(intentService)
             }
         }
     }
 
-    @SuppressLint("MissingPermission")
-    fun vibratePhone() {
-        val pattern = longArrayOf(0, 200, 100, 300)
-        vibrator?.let {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                it.vibrate(VibrationEffect.createWaveform(pattern, 0))
-            } else {
-                //deprecated in API 26
-                it.vibrate(pattern, 0)
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    fun stopVibratePhone() {
-        vibrator?.cancel()
-    }
-
-    private fun startMediaPlayer() {
-        if (mediaPlayer != null) {
-            mediaPlayer!!.stop()
-            mediaPlayer!!.release()
-            mediaPlayer = null
-        }
-
-        mediaPlayer = MediaPlayer.create(MainActivity.activity, R.raw.rington)
-        mediaPlayer?.start()
-
-        mediaPlayer?.setOnCompletionListener(MediaPlayer.OnCompletionListener() {
-            it.release()
-        })
-    }
 
 
-    private fun releaseMediaPlayer() {
-        try {
-            mediaPlayer?.run {
-                if (isPlaying) stop()
-                release()
-            }
-        } finally {
-            mediaPlayer = null
-        }
-    }
 }
-
 
 const val TAG = "GeofenceReceiver"
