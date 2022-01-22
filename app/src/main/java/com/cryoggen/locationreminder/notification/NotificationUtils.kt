fb24e5e0620b648a6b34714d98d73b5b262fdcd8
Sergey-Svetlov <cryoggen@gmail.com>
2022-01-22 23:03:16 +0300

    Update NotificationUtils.kt

--- app/src/main/java/com/cryoggen/locationreminder/notification/NotificationUtils.kt
+++ app/src/main/java/com/cryoggen/locationreminder/notification/NotificationUtils.kt
@@ -1,5 +1,6 @@
 package com.cryoggen.locationreminder.addeditreminder
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -18,11 +19,11 @@ import com.cryoggen.locationreminder.geofence.GeofencingConstants
  * We need to create a NotificationChannel associated with our CHANNEL_ID before sending a
  * notification.
  */
-fun createChannel(context: Context) {
+fun createChannelGeofenceEnterNotifications(context: Context) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
         val notificationChannel = NotificationChannel(
-            CHANNEL_ID,
-            context.getString(R.string.channel_name),
+            CHANNEL_ENTER_IN_GEOFENCE_ID,
+            context.getString(R.string.channel_name_enter_in_geofence),
             NotificationManager.IMPORTANCE_HIGH
         )
             .apply {
@@ -34,7 +35,33 @@ fun createChannel(context: Context) {
             lightColor = Color.RED
             enableVibration(false)
             description =
-                context.getString(R.string.notification_channel_description)
+                context.getString(R.string.enter_in_geofence_notifications_description)
+            setSound(null, null)
+
+        }
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+}
+
+fun createChannelGeofenceStatusNotification(context: Context) {
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val notificationChannel = NotificationChannel(
+            CHANNEL_GEOFENCE_STATUS_ID,
+            context.getString(R.string.channel_name_geofence_status),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+            .apply {
+                setShowBadge(false)
+            }
+
+        notificationChannel.apply {
+            enableLights(true)
+            lightColor = Color.RED
+            enableVibration(false)
+            description =
+                context.getString(R.string.geofence_status_notification_channel_description)
             setSound(null, null)
 
         }
@@ -48,24 +75,25 @@ fun createChannel(context: Context) {
  * entered notification.  It sends a custom notification based on the name string associated
  * with the LANDMARK_DATA from GeofencingConstatns in the GeofenceUtils file.
  */
-fun NotificationManager.sendGeofenceEnteredNotification(context: Context, GeofenceId: String) {
-
+fun sendGeofenceEnteredNotification(context: Context, reminderId: String): Notification {
     val contentIntent = Intent(context, MainActivity::class.java)
-    contentIntent.action = GeofencingConstants.ACTION_CLOSE_NOTIFICATION
-    contentIntent.putExtra(GeofencingConstants.EXTRA_GEOFENCE_INDEX, GeofenceId)
-    val contentPendingIntent = PendingIntent.getActivity(
+    contentIntent.action = ACTION_CLOSE_NOTIFICATION_ENTER_IN_GEOFENCE_ID
+    contentIntent.putExtra(GeofencingConstants.EXTRA_GEOFENCE_INDEX, reminderId)
+
+    val pendingIntent = PendingIntent.getActivity(
         context,
-        NOTIFICATION_ID,
+        NOTIFICATION_ENTER_IN_GEOFENCE_ID,
         contentIntent,
         PendingIntent.FLAG_UPDATE_CURRENT
     )
 
+
     val closeNotificationIntent = Intent(context, GeofenceBroadcastReceiver::class.java)
-    closeNotificationIntent.action = GeofencingConstants.ACTION_CLOSE_NOTIFICATION
+    closeNotificationIntent.action = ACTION_CLOSE_NOTIFICATION_ENTER_IN_GEOFENCE_ID
 
     val closePendingIntent = PendingIntent.getBroadcast(
         context,
-        111,
+        NOTIFICATION_ENTER_IN_GEOFENCE_ID,
         closeNotificationIntent,
         PendingIntent.FLAG_UPDATE_CURRENT
     )
@@ -73,18 +101,18 @@ fun NotificationManager.sendGeofenceEnteredNotification(context: Context, Geofen
 
     // We use the name resource ID from the LANDMARK_DATA along with content_text to create
     // a custom message when a Geofence triggers.
-    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+    val notification = NotificationCompat.Builder(context, CHANNEL_ENTER_IN_GEOFENCE_ID)
         .setContentTitle(context.getString(R.string.app_name))
         .setContentText(
             context.getString(
                 R.string.content_text
             )
         )
-        .setPriority(NotificationCompat.PRIORITY_LOW)
-        .setContentIntent(contentPendingIntent)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setContentIntent(pendingIntent)
         .addAction(
             R.drawable.ic_notification_open_button_foreground,
-            context.resources.getString(R.string.open), contentPendingIntent
+            context.resources.getString(R.string.open), pendingIntent
         )
         .addAction(
             R.drawable.ic_notification_close_button_foreground,
@@ -92,22 +120,77 @@ fun NotificationManager.sendGeofenceEnteredNotification(context: Context, Geofen
         )
         .setSmallIcon(R.drawable.ic_notification)
         .setAutoCancel(true)
-        .setColor(context.resources.getColor(R.color.secondaryDarkColor))
+        .setColor(context.resources.getColor(R.color.colorRed))
         .setCategory(NotificationCompat.CATEGORY_ALARM)
         .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
         .setOngoing(true)
         .setDefaults(0)
         .setSound(null)
+        .build()
 
-    notify(NOTIFICATION_ID, builder.build())
+    return notification
 
 }
 
-fun NotificationManager.cancelNotifications() {
+fun sendNotificationStatus(context: Context, text: String): Notification {
+    val pendingIntent: PendingIntent =
+        Intent(context, MainActivity::class.java).let { notificationIntent ->
+            PendingIntent.getActivity(
+                context,
+                NOTIFICATION_GEOFENCE_STATUS_ID,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        }
+
+       val disableButtonPendingIntent =
+        Intent(context, GeofenceBroadcastReceiver::class.java).let { notificationIntent ->
+            notificationIntent.action = ACTION_CLOSE_NOTIFICATION_GEOFENCE_STATUS
+            PendingIntent.getBroadcast(
+                context,
+                NOTIFICATION_GEOFENCE_STATUS_ID,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        }
+
+
+
+    val notification: Notification =
+        NotificationCompat.Builder(context, CHANNEL_GEOFENCE_STATUS_ID)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentTitle(context.getString(R.string.app_name))
+            .setColor(context.resources.getColor(R.color.colorRed))
+            .setContentText(text)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentIntent(pendingIntent)
+            .setTicker(text)
+            .setAutoCancel(true)
+            .addAction(
+                R.drawable.ic_notification_close_button_foreground,
+                context.resources.getString(R.string.disable), disableButtonPendingIntent
+            )
+            .build()
+
+    return notification
+}
+
+
+fun NotificationManager.cancelAllNotifications() {
     cancelAll()
 }
 
-const val NOTIFICATION_ID = 33
-private const val CHANNEL_ID = "GeofenceChannel"
+const val ACTION_CLOSE_NOTIFICATION_ENTER_IN_GEOFENCE_ID=
+    "com.cryoggen.action.ACTION_CLOSE_NOTIFICATION_ENTER_IN_GEOFENCE_ID"
+
+const val ACTION_CLOSE_NOTIFICATION_GEOFENCE_STATUS =
+    "com.cryoggen.action.ACTION_CLOSE_NOTIFICATION_GEOFENCE_STATUS"
+
+const val CHANNEL_ENTER_IN_GEOFENCE_ID = "CHANNEL_ENTER_IN_GEOFENCE_ID"
+const val CHANNEL_GEOFENCE_STATUS_ID = "CHANNEL_GEOFENCE_STATUS_ID"
+
+const val NOTIFICATION_ENTER_IN_GEOFENCE_ID = 77
+const val NOTIFICATION_GEOFENCE_STATUS_ID = 33
+
 
 
