dced3d58c1fc780c5ab0dd13e5cd4110e8754bcb
Sergey-Svetlov <cryoggen@gmail.com>
2022-01-29 20:15:39 +0300

    Add and correct comments

--- app/src/main/java/com/cryoggen/locationreminder/reciver/ReminderBroadcastReceiver.kt
+++ app/src/main/java/com/cryoggen/locationreminder/reciver/ReminderBroadcastReceiver.kt
@@ -4,26 +4,19 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.cryoggen.locationreminder.addeditreminder.*
+import com.cryoggen.locationreminder.notification.ACTION_CLOSE_NOTIFICATION_ENTER_IN_GEOFENCE_ID
+import com.cryoggen.locationreminder.notification.ACTION_CLOSE_NOTIFICATION_GEOFENCE_STATUS
+import com.cryoggen.locationreminder.notification.NOTIFICATION_ENTER_IN_GEOFENCE_ID
 import com.cryoggen.locationreminder.servises.*
 
 
-
-/*
- * Triggered by the Geofence.  Since we only have one active Geofence at once, we pull the request
- * ID from the first Geofence, and locate it within the registered landmark data in our
- * GeofencingConstants within GeofenceUtils, which is a linear string search. If we had  very large
- * numbers of Geofence possibilities, it might make sense to use a different data structure.  We
- * then pass the Geofence index into the notification, which allows us to have a custom "found"
- * message associated with each Geofence.
- */
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
 
         if (intent.action == ACTION_CLOSE_NOTIFICATION_GEOFENCE_STATUS) {
-            val intent = Intent(context, RemindersService::class.java)
-            context.stopService(intent)
+            val intentStopService = Intent(context, RemindersService::class.java)
+            context.stopService(intentStopService)
         }
 
 
@@ -42,4 +35,4 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
 }
 
-const val TAG = "GeofenceReceiver"
+
